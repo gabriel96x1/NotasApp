4054daa406bfb00077aa4e01463da2930d9c62b0
gabriel96x1 <gabriel96x1@gmail.com>
2022-01-09 17:55:40 -0600

    new view

--- app/src/main/java/com/another/notasapp/recyclerNotas.java
+++ app/src/main/java/com/another/notasapp/recyclerNotas.java
@@ -1,14 +1,46 @@
 package com.another.notasapp;
 
+import static java.security.AccessController.getContext;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.security.AccessController;
+
 public class recyclerNotas extends AppCompatActivity {
+    private RecyclerView rView;
+    private NotesAdapter rAdapter;
+    private static final String[] myDataSet = {
+            "PHP",
+            "Javascript",
+            "Go",
+            "Python"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler_notas2);
+
+        rView = findViewById(R.id.recyclerViewNotas);
+
+        // Esta línea mejora el rendimiento, si sabemos que el contenido
+        // no va a afectar al tamaño del RecyclerView
+        rView.setHasFixedSize(true);
+
+        // Nuestro RecyclerView usará un linear layout manager
+        GridLayoutManager layoutManager = new GridLayoutManager();
+        rView.setLayoutManager(layoutManager);
+
+        // Asociamos un adapter (ver más adelante cómo definirlo)
+        rAdapter = new MyAdapter(myDataSet);
+        rView.setAdapter(rAdapter);
+
+
+
+
     }
 }
